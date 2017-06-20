4fb86b234c094520a297bae5559a0acf48c21140
Macky Beltran <mackybeltran1@gmail.com>
2017-06-20 05:28:03 -0400

    deploy ready

---
+++ pages/public/components/Photolist.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
+
+const photos = [{path: './public/images/photos/antwerpen14.jpg', caption: 'Antwerp 2014'},
+                {path: './public/images/photos/bergenorway14.jpg', caption: 'Norway 2014'},
+                {path: './public/images/photos/ed13b.jpg', caption: 'Edinburgh 2013'},
+                {path: './public/images/photos/ed13c.jpg', caption: 'Edinburgh 2013'},
+                {path: './public/images/photos/edin13.jpg', caption: 'Edinburgh 2013'},
+                {path: './public/images/photos/gram13.jpg', caption: 'Manila 2013'},
+                {path: './public/images/photos/gramercy.jpg', caption: 'Manila 2013'},
+                {path: './public/images/photos/halfcrowd.jpg', caption: 'Edinburgh 2013'},
+                {path: './public/images/photos/jardin16.jpg', caption: 'Manila 2016'},
+                {path: './public/images/photos/jardin16b.jpg', caption: 'Manila 2016'},
+                {path: './public/images/photos/jardin16c.jpg', caption: 'Manila 2016'},
+                {path: './public/images/photos/korea15.jpg', caption: 'South Korea 2015'}]
+
+class Photolist extends React.Component {
+
+  render() {
+    return (
+      <Carousel>
+        {photos.map((photo, index) => {
+          return (
+            <Carousel.Item key={index}>
+               <img width={900} height={500} src={photo.path}/>
+                <Carousel.Caption>
+                  <p>{photo.caption}</p>
+                </Carousel.Caption>
+            </Carousel.Item>)
+        })}
+      </Carousel>
+    )
+  }
+}
+
+export default Photolist
---
+++ pages/public/components/Videolist.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
+
+const videos = [{path: 'https://www.youtube.com/embed/2gOeuvX_JGY', caption: 'Demo Reel'},
+                {path: 'https://www.youtube.com/embed/2cyt3ndRWgk', caption: 'Street Magic'},
+                {path: 'https://www.youtube.com/embed/d1krzn_nH_c', caption: 'Old Demo Reel'}]
+
+class Videolist extends React.Component {
+
+  render() {
+    return (
+      <Carousel>
+        {videos.map((video, index) => {
+          return (
+            <Carousel.Item key={index}>
+               <iframe width={420} height={315} src={video.path} allowFullScreen/>
+                <Carousel.Caption>
+                  <p>{video.caption}</p>
+                </Carousel.Caption>
+            </Carousel.Item>)
+        })}
+      </Carousel>
+    )
+  }
+}
+
+export default Videolist
