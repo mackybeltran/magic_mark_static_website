de3ba8261e137d5b061de79832aa401792e0dfc3
Macky Beltran <mackybeltran1@gmail.com>
2017-06-21 00:37:56 -0400

    responsive design for mobile

---
+++ pages/public/components/Services.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap'
+
+class Services extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <Modal.Body>
+                  <h4>Services</h4>
+
+                  <h6>Parlour show </h6>
+                  <p>I have a unique parlour type show (60-100 people)  that has been astounding thousands of people around the globe.
+                    My parlour show is versatile and can be done anywhere, any time.
+                  </p>
+                  <b/>
+                  <p>The entire act lasts about 45 minutes, and can be easily shortened or lengthened to meet any time requirements that you may have.
+                  </p>
+                  <b/>
+                  <p>The show is suitable for any age group. The type of material in the parlour show often depends on the average age of the audience.
+                    I can do a family show where the magic used utilizes very colorful and visual effects and I use props that children can relate to.
+                    For adults, I show them impossible tricks that will be sure to blow and entertain their minds.
+                  </p>
+                  <b/>
+                  <p>This show is a perfect addition to your birthday party, trade show, or corporate function.
+                  </p>
+                  <b/>
+                  <h6>  Close-up Magic Show </h6>
+                  <b/>
+                  <p> Close up magic is a show done in close proximity of the guests.
+                    My close-up show takes takes place in the spectator’s very own hands. I use the art called prestidigitation,
+                    which is the highest and hardest form of magic and you can experience it first hand.
+                  </p>
+                  <b/>
+                  <p>This show is perfect for events where you want the entertainment to blend in and allow your guests to do what they want.
+                    I am able to perform this show for several hours, without repeating material. As I mingle amongst your guests, I will perform miracles for them in small groups.
+                  </p>
+                  <b/>
+                  <p>This type of show is perfect for restaurants, lounge rooms, cruise ships, hospitality suites,
+                    corporate and private events and weddings.
+                  </p>
+                  <b/>
+                  <p>The material in this show has people laughing, screaming, cheering, and applauding.
+                    Sometimes, the responses of these small groups can fill up the entire venue.
+                  </p>
+                  <b/>
+                  <p>Magic in general is a great way to entertain guests, build interest and create foot traffic at any venue.
+                  </p>
+                </Modal.Body>
+
+      </div>
+      )
+    }
+
+
+  }
+export default Services
